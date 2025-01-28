7abb1cc45c1b0d4d67c1528c5318723162308f67
Russ <Russ@DESKTOP-T3B2P5I>
2025-01-28 14:22:57 -0500

    New code

---
+++ Wk4/src/wk4/WK4.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package wk4;
+
+/**
+ * 
+ */
+public class WK4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
